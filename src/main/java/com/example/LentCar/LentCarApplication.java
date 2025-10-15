a75fbf0c0e2b22322f96845be83f9542d82c661f
simile <tommy_1222@naver.com>
2025-10-15 14:50:26 +0900

    First commit

---
+++ src/main/java/com/example/LentCar/LentCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.LentCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LentCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LentCar/LentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LentCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LentCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
