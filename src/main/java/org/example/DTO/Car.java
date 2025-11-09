71755770d354797970194282dff4c4d97294c66f
skao327 <skao327@naver.com>
2025-11-09 16:44:48 +0900

    거리 변수 추가
    Reordered fields and added currentMileage property with getter and setter methods.

--- src/main/java/org/example/DTO/Car.java
+++ src/main/java/org/example/DTO/Car.java
@@ -4,23 +4,34 @@ public class Car {
     private String carID;
     private String modelName;
     private String carPlateNumber;
-    private Integer baseRentalPrice;
-    private String parkingLocation;
     private boolean isAvailable;
+
+    private String parkingLocation;
+
+    private Integer baseRentalPrice;
+    private double currentMileage;
+    
 
     public Car(String carID, String modelName, Integer baseRentalPrice) {
         this.carID = carID;
         this.modelName = modelName;
         this.baseRentalPrice = baseRentalPrice;
         this.isAvailable = true;
+        this.currentMileage = 0.0;
     }
 
     public void setAvailable(boolean available) {
         this.isAvailable = available;
     }
+    public setMileage(double mileage) {
+        this.currentMileage = mileage;
+    }
+    
     public boolean isAvailble() {return isAvailable;}
     public String getCarID() {return carID;}
     public String getModelName() {return modelName;}
     public String getCarPlateNumber() {return carPlateNumber;}
+    
     public Integer getBaseRentalPrice() {return baseRentalPrice;}
+    public double getCurrentMileage() {return currentMileage;}
 }
