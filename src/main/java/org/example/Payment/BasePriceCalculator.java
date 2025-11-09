156c387434e61bd712c630fe94a416a5e4635dd8
남예빈 <skao327@naver.com>
2025-11-09 19:06:22 +0900

    데코레이터 패턴으로 가격 책정  1차 구현

---
+++ src/main/java/org/example/Payment/BasePriceCalculator.java
@@ -0,0 +1,13 @@
+package org.example.Payment;
+
+public class BasePriceCalculator implements PriceCalculator {
+    private int basePrice;
+
+    public BasePriceCalculator(int basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public int calculatePrice() {
+        return this.basePrice;
+    }
+}
