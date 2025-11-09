70082fea22e8e4504a2ef54b7b16b011d30af83a
skao327 <skao327@naver.com>
2025-11-09 14:25:47 +0900

    Car 클래스

--- src/main/java/org/example/DTO/Car.java
+++ src/main/java/org/example/DTO/Car.java
@@ -2,7 +2,25 @@ package org.example.DTO;
 
 public class Car {
     private String carId;
+    private String modelName;
     private String carPlateNumber;
     private Integer price;
     private String parkingLocation;
+    private boolean isAvailable;
+
+    public Car(String carID, String modelName, Integer price) {
+        this.carID = carID;
+        this.modelName = modelName;
+        this.price = price;
+        this.isAvailable = true;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+    public boolean isAvailble() {return isAvailable;}
+    public String getCarID() {return carID;}
+    public String getModelName() {return modelName;}
+    public String getCarPlateNumber() {return carPlateNumber;}
+    public Integer getprice() {return price;}
 }
