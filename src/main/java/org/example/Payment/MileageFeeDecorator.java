1e50b14d5beaac5e023c39f98c39b6d0c3978bd7
skao327 <skao327@naver.com>
2025-12-02 21:29:51 +0900

    결제 가격 안내 문구 수정

--- src/main/java/org/example/Payment/MileageFeeDecorator.java
+++ src/main/java/org/example/Payment/MileageFeeDecorator.java
@@ -11,7 +11,7 @@ public class MileageFeeDecorator extends PriceDecorator {
     public int calculatePrice() {
         int basePrice = super.calculatePrice();
         int mileageFee = (int) (mileage*ratePerKm);
-        System.out.println(" [결제 가격] 기본 대여료: " + basePrice + "원, 주행 시간(" + mileage + "): " + mileageFee);
+        System.out.println(" [결제 가격] 기본 대여료: " + basePrice + "원, 주행 거리(" + mileage + "): " + mileageFee);
         return basePrice + mileageFee;
     }
 }
